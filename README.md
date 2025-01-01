7afda87a4ba9060cdf230bc690167a52058a7ed7
soumya-99 <soumyadeepmondal1234@gmail.com>
2025-01-01 13:32:43 +0530

    release with keytool added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bill 365
 
 `App Name Code - Bill365_2.2_17Jul24_L`
+
+"react-native-vision-camera": "^4.0.3"
--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,6 +1,6 @@
 import { Alert, Linking, Platform, StyleSheet, Text, ToastAndroid, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { Camera, Code, CodeScanner, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera'
+// import { Camera, Code, CodeScanner, useCameraDevice, useCameraPermission, useCodeScanner } from 'react-native-vision-camera'
 import { CommonActions, useNavigation } from '@react-navigation/native'
 import Sound from 'react-native-sound'
 import { productHeader, productHeaderDark } from "../resources/images"
@@ -13,101 +13,101 @@ import { getUniqueWithMaxCount } from '../utils/getUniqueWithMaxCount'
 const SAMPLING_RATE = 16
 
 function CameraScreen() {
-    const device = useCameraDevice('back')
-    const { hasPermission, requestPermission } = useCameraPermission()
-    const [scannedCode, setScannedCode] = useState<string>(() => "")
-    const [scanned, setScanned] = useState(false)
-    const navigation = useNavigation()
+    // const device = useCameraDevice('back')
+    // const { hasPermission, requestPermission } = useCameraPermission()
+    // const [scannedCode, setScannedCode] = useState<string>(() => "")
+    // const [scanned, setScanned] = useState(false)
+    // const navigation = useNavigation()
     const theme = usePaperColorScheme()
 
-    let samplingBarcodes: Code[][] = [[]]
-    let barcodes: string[] = []
+    // let samplingBarcodes: Code[][] = [[]]
+    // let barcodes: string[] = []
 
-    Sound.setCategory("Alarm")
+    // Sound.setCategory("Alarm")
 
-    var beep = new Sound('beep.mp3', Sound.MAIN_BUNDLE, (error) => {
-        if (error) {
-            console.log('failed to load the sound', error)
-            return
-        }
-    })
+    // var beep = new Sound('beep.mp3', Sound.MAIN_BUNDLE, (error) => {
+    //     if (error) {
+    //         console.log('failed to load the sound', error)
+    //         return
+    //     }
+    // })
 
-    beep.setVolume(1)
+    // beep.setVolume(1)
 
-    const codeScanner: CodeScanner = useCodeScanner({
-        codeTypes: ['code-128', 'code-39', 'code-93', 'codabar', 'ean-13', 'ean-8', 'itf', 'upc-e', 'upc-a'],
-        onCodeScanned: (codes: Code[]) => {
-            if (!scanned) {
-                console.log(`Scanned ${codes.length} codes!`)
-                samplingBarcodes.push(codes)
+    // const codeScanner: CodeScanner = useCodeScanner({
+    //     codeTypes: ['code-128', 'code-39', 'code-93', 'codabar', 'ean-13', 'ean-8', 'itf', 'upc-e', 'upc-a'],
+    //     onCodeScanned: (codes: Code[]) => {
+    //         if (!scanned) {
+    //             console.log(`Scanned ${codes.length} codes!`)
+    //             samplingBarcodes.push(codes)
 
-                if (samplingBarcodes.length >= SAMPLING_RATE) {
-                    setScanned(true)
+    //             if (samplingBarcodes.length >= SAMPLING_RATE) {
+    //                 setScanned(true)
 
-                    for (const codes of samplingBarcodes) {
-                        for (const code of codes) {
-                            console.log(code.type, code.value)
+    //                 for (const codes of samplingBarcodes) {
+    //                     for (const code of codes) {
+    //                         console.log(code.type, code.value)
 
-                            barcodes.push(code.value)
+    //                         barcodes.push(code.value)
 
-                            console.log("CODEEEE====", code)
-                            console.log("CODEEEESSSS====", codes)
-                        }
-                    }
+    //                         console.log("CODEEEE====", code)
+    //                         console.log("CODEEEESSSS====", codes)
+    //                     }
+    //                 }
 
-                    const uniqueWithMaxCount = getUniqueWithMaxCount(barcodes)
-                    console.log("UNIQUE CODE +++++++======", uniqueWithMaxCount)
+    //                 const uniqueWithMaxCount = getUniqueWithMaxCount(barcodes)
+    //                 console.log("UNIQUE CODE +++++++======", uniqueWithMaxCount)
 
-                    navigation.dispatch(
-                        CommonActions.navigate({
-                            name: navigationRoutes.categoryProductsScreen,
-                            params: {
-                                barcode: uniqueWithMaxCount,
-                                navigatedFromCamera: true
-                            }
-                        })
-                    )
-                    setScannedCode(uniqueWithMaxCount)
+    //                 navigation.dispatch(
+    //                     CommonActions.navigate({
+    //                         name: navigationRoutes.categoryProductsScreen,
+    //                         params: {
+    //                             barcode: uniqueWithMaxCount,
+    //                             navigatedFromCamera: true
+    //                         }
+    //                     })
+    //                 )
+    //                 setScannedCode(uniqueWithMaxCount)
 
-                    beep.play(success => {
-                        if (success) {
-                            console.log("BEEP PLAYED.")
-                        } else {
-                            console.log("BEEP ERROR!")
-                        }
-                    })
-                }
+    //                 beep.play(success => {
+    //                     if (success) {
+    //                         console.log("BEEP PLAYED.")
+    //                     } else {
+    //                         console.log("BEEP ERROR!")
+    //                     }
+    //                 })
+    //             }
 
-            }
-        }
-    })
+    //         }
+    //     }
+    // })
 
-    useEffect(() => {
-        const cameraPermissionRequest = () => {
-            if (!hasPermission) {
-                requestPermission().then(res => {
-                    if (res) {
-                        console.log("REQUEST GRANTED...", res)
-                    } else {
-                        console.log("REQUEST REJECTED...", res)
-                        Alert.alert(
-                            "Allow Permissions",
-                            "Click Open Settings: Go to Permissions -> Allow Camera access.",
-                            [
-                                { text: "Open Settings", onPress: () => Linking.openSettings() },
-                                { text: "Later", onPress: () => null },
-                            ],
-                        )
-                    }
-                }).catch(err =>
-                    console.log("SOME PROBLEM DETECTED WHILE PERMISSION OF CAMERA GIVING...", err)
-                )
-            }
-        }
-        if (Platform.OS === "android") {
-            cameraPermissionRequest()
-        }
-    }, [])
+    // useEffect(() => {
+    //     const cameraPermissionRequest = () => {
+    //         if (!hasPermission) {
+    //             requestPermission().then(res => {
+    //                 if (res) {
+    //                     console.log("REQUEST GRANTED...", res)
+    //                 } else {
+    //                     console.log("REQUEST REJECTED...", res)
+    //                     Alert.alert(
+    //                         "Allow Permissions",
+    //                         "Click Open Settings: Go to Permissions -> Allow Camera access.",
+    //                         [
+    //                             { text: "Open Settings", onPress: () => Linking.openSettings() },
+    //                             { text: "Later", onPress: () => null },
+    //                         ],
+    //                     )
+    //                 }
+    //             }).catch(err =>
+    //                 console.log("SOME PROBLEM DETECTED WHILE PERMISSION OF CAMERA GIVING...", err)
+    //             )
+    //         }
+    //     }
+    //     if (Platform.OS === "android") {
+    //         cameraPermissionRequest()
+    //     }
+    // }, [])
 
     return (
         <View style={[StyleSheet.absoluteFill, { backgroundColor: theme.colors.secondaryContainer }]}>
@@ -121,7 +121,7 @@ function CameraScreen() {
                     Scan Barcode
                 </HeaderImage>
             </View>
-            <View style={{
+            {/* <View style={{
                 width: SCREEN_WIDTH / 1.14,
                 height: SCREEN_HEIGHT / 3.4,
                 borderWidth: 15,
@@ -130,7 +130,7 @@ function CameraScreen() {
                 alignSelf: "center"
             }}>
                 <Camera isActive={true} device={device} codeScanner={codeScanner} style={[StyleSheet.absoluteFill]} focusable enableZoomGesture collapsable />
-            </View>
+            </View> */}
         </View>
     )
 }
