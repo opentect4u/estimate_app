8817c78e01c523446aa8d305a4d65431cc68efa3
somnathsynergic <somnath@synergicsoftek.in>
2025-01-03 11:08:42 +0530

    commit with credit

--- api/report.py
+++ api/report.py
@@ -13,7 +13,7 @@ async def Bill_sum(bill_sum:DashBoard):
     conn = connect()
     cursor = conn.cursor()
 
-    query = f"SELECT COUNT(a.receipt_no)total_bills, SUM(a.net_amt)amount_collected FROM td_receipt a, md_user b,md_branch c,md_company d WHERE a.created_by=b.user_id and b.br_id=c.id and b.comp_id=d.id and d.id={bill_sum.comp_id} and c.id={bill_sum.br_id} and a.trn_date='{bill_sum.trn_date}' and a.created_by='{bill_sum.user_id}' and a.receipt_no not in (select receipt_no from td_receipt_cancel_new)"
+    query = f"SELECT COUNT(a.receipt_no)total_bills, SUM(a.net_amt)amount_collected FROM td_receipt a, md_user b,md_branch c,md_company d WHERE a.created_by=b.user_id and b.br_id=c.id and b.comp_id=d.id and d.id={bill_sum.comp_id} and c.id={bill_sum.br_id} and a.trn_date='{bill_sum.trn_date}' and a.created_by='{bill_sum.user_id}' and a.receipt_no in (select receipt_no from td_item_sale where cancel_flag=0)"
 
     cursor.execute(query)
     records = cursor.fetchall()
