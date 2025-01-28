d9ec5888872a6e7fbcd71afd6c9c47ca31d5b619
soumya-99 <soumyadeepmondal1234@gmail.com>
2025-01-28 19:42:49 +0530

    estimate app build

--- src/Address/baseURL.js
+++ src/Address/baseURL.js
@@ -1,4 +1,5 @@
 // export const url = "https://apibilling.opentech4u.co.in:3005"
 // export const url = "https://apibilling.opentech4u.co.in:3010";
-export const url = "http://202.21.38.178:3001";
+// export const url = "http://202.21.38.178:3001";
+export const url = "http://98.130.88.168:3001";
 // export const url = "http://localhost:3005";
