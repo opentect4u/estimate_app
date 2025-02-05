5a521a284affe128410243df1fcb9c71abcf6f2e
somnathsynergic <somnath@synergicsoftek.in>
2025-02-05 17:48:31 +0530

    commit with menus

--- V2/api/report.py
+++ V2/api/report.py
@@ -459,6 +459,7 @@ async def due_report(data:DueReport):
     conn = connect()
     cursor = conn.cursor()
     query = f"select ifnull(b.cust_name,'NA')cust_name, a.phone_no, Sum(due_amt) - Sum(paid_amt)due_amt from td_recovery_new a,md_customer b where a.comp_id = b.comp_id and a.phone_no = b.phone_no and a.comp_id = {data.comp_id} and a.br_id = {data.br_id} and a.recover_dt <= '{data.date}' GROUP BY b.cust_name,a.phone_no having due_amt>0"
+    print(query)
     cursor.execute(query)
     records = cursor.fetchall()
     result = createResponse(records, cursor.column_names, 1)
